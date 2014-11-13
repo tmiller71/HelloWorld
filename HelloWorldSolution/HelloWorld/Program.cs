886413470acd516c11ffecb1847691e0e4fd0314
tmiller71 <tmiller71@gmail.com>
2014-11-13 16:56:51 -0500

    Initial commit

---
+++ HelloWorldSolution/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n\nPress any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
